6247a399dc12ff7fbbd0219bbf7f9dc884f4e17c
raahii <piyo56.net@gmail.com>
2020-03-21 12:45:15 +0900

    Add object types: integer, bool, null. #3

---
+++ object/object.go
@@ -0,0 +1,39 @@
+package object
+
+import "fmt"
+
+// general object interface
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// Integer
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+// Boolean
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+// Null
+type Null struct{}
+
+func (b *Null) Type() ObjectType { return NULL_OBJ }
+func (b *Null) Inspect() string  { return "null" }
