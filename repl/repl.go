a0cf234cb9d6cabaa7f3507fcd86ab1b28066fc5
raahii <piyo56.net@gmail.com>
2020-03-26 13:16:28 +0900

    Now RPPL(R-Parse-P-L) evolved into REPL(R-Evaluate-P-L). #3

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"monkey/evalator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -53,7 +54,10 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evalator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
