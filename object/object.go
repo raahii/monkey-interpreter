9f52424152f4e23d99bbee8bd49621ad62693c66
raahii <piyo56.net@gmail.com>
2020-03-28 16:10:31 +0900

    Make it possible to evaluate function literal. #3

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 // general object interface
 type ObjectType string
@@ -11,11 +16,14 @@ type Object interface {
 }
 
 const (
-	INTEGER_OBJ      = "INTEGER"
-	BOOLEAN_OBJ      = "BOOLEAN"
-	NULL_OBJ         = "NULL"
+	INTEGER_OBJ   = "INTEGER"
+	BOOLEAN_OBJ   = "BOOLEAN"
+	FUNCITION_OBJ = "FUNCTION"
+
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ        = "ERROR"
+
+	NULL_OBJ  = "NULL"
+	ERROR_OBJ = "ERROR"
 )
 
 // Integer
@@ -34,11 +42,31 @@ type Boolean struct {
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
-// Null
-type Null struct{}
+// Function
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
 
-func (b *Null) Type() ObjectType { return NULL_OBJ }
-func (b *Null) Inspect() string  { return "null" }
+func (f *Function) Type() ObjectType { return FUNCITION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
 
 // Return Value
 type ReturnValue struct {
@@ -47,6 +75,12 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+// Null
+type Null struct{}
+
+func (b *Null) Type() ObjectType { return NULL_OBJ }
+func (b *Null) Inspect() string  { return "null" }
 
 // Error
 type Error struct {
